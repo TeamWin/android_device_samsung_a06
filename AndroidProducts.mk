a2b7ccce9a4a6fc0f3916881b80ed245e8034076
Andrea Toska <toskaandrea@gmail.com>
2024-12-10 09:35:56 +0100

    A06: init

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a06.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a06-user \
+    twrp_a06-userdebug \
+    twrp_a06-eng
---
+++ README.md
@@ -0,0 +1,2 @@
+# twrp_device_samsung_a06
+TWRP Device Tree For Samsung Galaxy A06 (MediaTek)
